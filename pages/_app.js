6ed195b96d904d520a004d8b2c8478b893e93be8
Eduardo Silva <educwb@gmail.com>
2021-08-31 02:49:09 -0300

    adicionando conteudo no _app.js para exibir em todas paginas

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <h1>Título Fixo</h1>
+    <Component {...pageProps} />
+  </> 
 }
 
 export default MyApp
